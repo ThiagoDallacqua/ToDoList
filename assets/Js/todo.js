65a5d97f4cc20b1550730e24207cca1ebbfafe08
Thiago Dallacqua <thiagodallacqua@outlook.com>
2016-10-26 14:52:20 -0300

    initial commit

---
+++ assets/Js/todo.js
@@ -0,0 +1,59 @@
+/**
+ * Created by thiago on 25/10/16.
+ */
+
+$("input[type='text']").css("display", "none");
+
+$("ul").on("click", "li", function () { //will add the event to all possible li's, indifferently if they exist at the first load of the page or not
+
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "li span", function (event) {
+
+
+    $(this).parent().fadeOut(500, function () { //remove the li which contains the referred span
+
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").on("keypress", function(event) {
+
+    if(event.which === 13){
+
+        var todoText = $(this).val();
+
+        $("ul").append("<li><span><i class='fa fa-trash-o' aria-hidden='true'></i></span> " + todoText + "</li>");
+
+        $(this).val("");
+    }
+});
+
+$(".fa-plus").click(function () {
+
+    if($("input[type='text']").css("display") === "none"){
+
+        $(this).css({
+
+            animationName: "faPlus",
+            animationDuration: "0.2s",
+            animationTimingFunction: "linear",
+            animationDelay: "0",
+            animationIterationCount: "1"
+        });
+    }else{
+
+        $(this).css({
+
+            animationName: "faPlusInverse",
+            animationDuration: "0.2s",
+            animationTimingFunction: "linear",
+            animationDelay: "0",
+            animationIterationCount: "1"
+        });
+    }
+
+    $("input[type='text']").fadeToggle("fast");
+});
